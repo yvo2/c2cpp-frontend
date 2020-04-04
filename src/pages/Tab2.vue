0d66068bfa9284649aec5b368e927655a16b1336
tabstma <marco.2000@icloud.com>
2020-04-04 20:36:26 +0200

    add order list

--- src/pages/Tab2.vue
+++ src/pages/Tab2.vue
@@ -2,14 +2,23 @@
   <ion-page class="ion-page">
     <ion-header>
       <ion-toolbar color="primary">
-        <ion-title>Ionic4 VueJS: Tab 2</ion-title>
+        <ion-title>Orders</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content padding>
       <ion-list lines="full" mode="ios">
-        <ion-item v-for="continent in continents" v-bind:key="continent.name">
-          {{continent.name}}
-        </ion-item>
+        <ion-card v-for="continent in continents" v-bind:key="continent.name">
+          <ion-card-header>
+            <ion-card-title>{{continent.name}}</ion-card-title>
+          </ion-card-header>
+
+          <ion-card-content>
+            I would like to get some milk and also other stufff please. Go out with doggo and do the garden. Thanks
+            
+          </ion-card-content>
+          <button class="red">Deny</button>
+          <button class="green">Accept</button>
+        </ion-card>
       </ion-list>
     </ion-content>
   </ion-page>
@@ -23,7 +32,10 @@ export default {
   apollo: {
     continents: gql`query {
       continents {
-        name
+        name,
+        countries{
+          name
+        }
       }
     }`,
   }
@@ -31,4 +43,32 @@ export default {
 </script>
 
 <style>
+
+button {
+  color: white;
+  text-align: center;
+  padding: 0px 15px;
+}
+
+ion-card button {
+  width: 50%;
+  height: 30px;
+}
+
+.green {
+  background-color: #2dd36f;
+}
+
+.red {
+  background-color: #eb445a;
+}
+
+.green:active {
+  background-color: #23a959;
+}
+
+.red:active {
+  background-color: #db1731;
+}
+
 </style>
