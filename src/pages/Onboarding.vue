b49230ccfa0011ea4713842f80a895970b70e311
imbenwolf <benjamineliaswolf@gmail.com>
2020-04-04 21:30:50 +0200

    Add onboarding component ui

---
+++ src/pages/Onboarding.vue
@@ -0,0 +1,316 @@
+<template>
+  <ion-page class="ion-page">
+    <ion-content padding>
+      <div class="form-structor">
+        <div class="signup" :class="{'slide-up': activeContainer === 'login'}">
+          <router-link to="/signup" tag="div">
+            <h2 class="form-title">
+              <span>or</span>Sign up
+            </h2>
+          </router-link>
+          <div class="form-holder">
+            <input type="text" class="input" placeholder="First name" />
+            <input type="text" class="input" placeholder="Last name" />
+            <input type="email" class="input" placeholder="Email" />
+            <input type="password" class="input" placeholder="Password" />
+          </div>
+          <button class="submit-btn">Sign up</button>
+        </div>
+        <div class="login" :class="{'slide-up': activeContainer === 'signup'}">
+          <div class="center">
+            <router-link to="/login" tag="div">
+              <h2 class="form-title">
+                <span>or</span>Log in
+              </h2>
+            </router-link>
+            <div class="form-holder">
+              <input type="email" class="input" placeholder="Email" />
+              <input type="password" class="input" placeholder="Password" />
+            </div>
+            <button class="submit-btn">Log in</button>
+          </div>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      activeContainer: null
+    };
+  },
+  watch: {
+    $route: {
+      handler: function(to) {
+        this.activeContainer = to.path.substr(1);
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    login() {
+      this.$router.push({ name: "tab1" });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.form-structor {
+  background-color: #222;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+
+  &::after {
+    content: "";
+    opacity: 0.8;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-repeat: no-repeat;
+    background-position: left bottom;
+    background-size: cover;
+    background-image: url("https://i.pinimg.com/736x/b8/09/22/b80922f6ea2daaf36a6627378662803b--deck-of-cards-phone-wallpapers.jpg");
+  }
+
+  .signup {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 65%;
+    z-index: 5;
+    transition: all 0.3s ease;
+
+    &.slide-up {
+      top: 5%;
+      transform: translate(-50%, 0%);
+      transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-holder,
+    &.slide-up .submit-btn {
+      opacity: 0;
+      visibility: hidden;
+    }
+
+    &.slide-up .form-title {
+      font-size: 1em;
+      cursor: pointer;
+    }
+
+    &.slide-up .form-title span {
+      margin-right: 5px;
+      opacity: 1;
+      visibility: visible;
+      transition: all 0.3s ease;
+    }
+
+    .form-title {
+      color: #fff;
+      font-size: 1.7em;
+      text-align: center;
+
+      span {
+        color: rgba(0, 0, 0, 0.4);
+        opacity: 0;
+        visibility: hidden;
+        transition: all 0.3s ease;
+      }
+    }
+
+    .form-holder {
+      border-radius: 15px;
+      background-color: #fff;
+      overflow: hidden;
+      margin-top: 50px;
+      opacity: 1;
+      visibility: visible;
+      transition: all 0.3s ease;
+
+      .input {
+        border: 0;
+        outline: none;
+        box-shadow: none;
+        display: block;
+        height: 30px;
+        line-height: 30px;
+        padding: 8px 15px;
+        border-bottom: 1px solid #eee;
+        width: 100%;
+        font-size: 12px;
+
+        &:last-child {
+          border-bottom: 0;
+        }
+        &::input-placeholder {
+          color: rgba(0, 0, 0, 0.4);
+        }
+      }
+    }
+
+    .submit-btn {
+      background-color: rgba(0, 0, 0, 0.4);
+      color: rgba(256, 256, 256, 0.7);
+      border: 0;
+      border-radius: 15px;
+      display: block;
+      margin: 15px auto;
+      padding: 15px 45px;
+      width: 100%;
+      font-size: 13px;
+      font-weight: bold;
+      cursor: pointer;
+      opacity: 1;
+      visibility: visible;
+      transition: all 0.3s ease;
+
+      &:hover {
+        transition: all 0.3s ease;
+        background-color: rgba(0, 0, 0, 0.8);
+      }
+    }
+  }
+
+  .login {
+    position: absolute;
+    top: 20%;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fff;
+    z-index: 5;
+    transition: all 0.3s ease;
+
+    &::before {
+      content: "";
+      position: absolute;
+      left: 50%;
+      top: -20px;
+      transform: translate(-50%, 0);
+      background-color: #fff;
+      width: 200%;
+      height: 250px;
+      border-radius: 50%;
+      z-index: 4;
+      transition: all 0.3s ease;
+    }
+
+    .center {
+      position: absolute;
+      top: calc(50% - 10%);
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 65%;
+      z-index: 5;
+      transition: all 0.3s ease;
+
+      .form-title {
+        color: #000;
+        font-size: 1.7em;
+        text-align: center;
+
+        span {
+          color: rgba(0, 0, 0, 0.4);
+          opacity: 0;
+          visibility: hidden;
+          transition: all 0.3s ease;
+        }
+      }
+
+      .form-holder {
+        border-radius: 15px;
+        background-color: #fff;
+        border: 1px solid #eee;
+        overflow: hidden;
+        margin-top: 50px;
+        opacity: 1;
+        visibility: visible;
+        transition: all 0.3s ease;
+
+        .input {
+          border: 0;
+          outline: none;
+          box-shadow: none;
+          display: block;
+          height: 30px;
+          line-height: 30px;
+          padding: 8px 15px;
+          border-bottom: 1px solid #eee;
+          width: 100%;
+          font-size: 12px;
+
+          &:last-child {
+            border-bottom: 0;
+          }
+          &::-webkit-input-placeholder {
+            color: rgba(0, 0, 0, 0.4);
+          }
+        }
+      }
+
+      .submit-btn {
+        background-color: #6b92a4;
+        color: rgba(256, 256, 256, 0.7);
+        border: 0;
+        border-radius: 15px;
+        display: block;
+        margin: 15px auto;
+        padding: 15px 45px;
+        width: 100%;
+        font-size: 13px;
+        font-weight: bold;
+        cursor: pointer;
+        opacity: 1;
+        visibility: visible;
+        transition: all 0.3s ease;
+
+        &:hover {
+          transition: all 0.3s ease;
+          background-color: rgba(0, 0, 0, 0.8);
+        }
+      }
+    }
+
+    &.slide-up {
+      top: 90%;
+      transition: all 0.3s ease;
+    }
+
+    &.slide-up .center {
+      top: 10%;
+      transform: translate(-50%, 0%);
+      transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-holder,
+    &.slide-up .submit-btn {
+      opacity: 0;
+      visibility: hidden;
+      transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-title {
+      font-size: 1em;
+      margin: 0;
+      padding: 0;
+      cursor: pointer;
+      transition: all 0.3s ease;
+    }
+
+    &.slide-up .form-title span {
+      margin-right: 5px;
+      opacity: 1;
+      visibility: visible;
+      transition: all 0.3s ease;
+    }
+  }
+}
+</style>
