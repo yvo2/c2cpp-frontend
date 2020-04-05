cd55decdc64b07cd931b8a6e3915ba221128e58e
imbenwolf <benjamineliaswolf@gmail.com>
2020-04-05 18:01:30 +0200

    Change icons in tabs navigation

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,22 +5,17 @@
         <ion-vue-router name="tab1"></ion-vue-router>
       </ion-tab>
 
-      <ion-tab tab="login" :routes="['login']">
-        <ion-vue-router name="login"></ion-vue-router>
-      </ion-tab>
-
       <ion-tab tab="tab2" :routes="['tab2']">
         <ion-vue-router name="tab2"></ion-vue-router>
       </ion-tab>
 
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1">
-          <ion-icon name="person" /> 
+          <ion-icon name="basket" /> 
         </ion-tab-button>
 
         <ion-tab-button tab="tab2">
-          <ion-icon name="map" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon name="people" />
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -28,14 +23,14 @@
 </template>
 
 <script>
-import { person, map } from "ionicons/icons";
+import { people, basket } from "ionicons/icons";
 import { addIcons } from "ionicons";
 
 addIcons({
-  "ios-person": person.ios,
-  "md-person": person.md,
-  "ios-map": map.ios,
-  "md-map": map.md
+  "ios-people": people.ios,
+  "md-people": people.md,
+  "ios-basket": basket.ios,
+  "md-basket": basket.md
 });
 
 export default {
