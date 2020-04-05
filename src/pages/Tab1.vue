141eb8fe5bd77493a7749aaaa412d4ad574f006a
imbenwolf <benjamineliaswolf@gmail.com>
2020-04-05 18:00:35 +0200

    Add navigation button in tab1 component

--- src/pages/Tab1.vue
+++ src/pages/Tab1.vue
@@ -1,7 +1,14 @@
 <template>
   <ion-page class="ion-page">
     <ion-content padding>
-      <ion-button @click="nextPage()">Next Page</ion-button>
+      <div class="navigation">
+      <a
+        :href="`https://www.google.com/maps/dir/?api=1&origin=&destination=bern&travelmode=bicycling`"
+        target="_blank"
+      >
+        <ion-button expand="full" color="success">Start navigation!</ion-button>
+      </a>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -18,5 +25,15 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.navigation {
+  position: absolute;
+  bottom: 10px;
+  padding: 0 10px;
+  width: 100%;
+}
+
+.navigation > a {
+  text-decoration: none;
+}
 </style>
